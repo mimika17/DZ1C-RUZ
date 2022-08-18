0c06823b4d9c10a7bb776ab03c891e37d9455f62
anna <mimika021981@gmail.com>
2022-08-18 19:30:28 +0500

    Z002

---
+++ z002/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 2
+//Напишите программу, которая принимает на вход три числа
+//и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write ("Введите  три целых числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
